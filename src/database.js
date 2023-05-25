1448f9e88b719bac7152dd5795ad3f2b9022954c
thewolf3304 <santiago.barboza@tecsup.edu.pe>
2023-05-24 19:20:08 -0500

    database catch conected => disconected

--- src/database.js
+++ src/database.js
@@ -3,4 +3,4 @@ const MONGODB_URI = require("./config");
 
 mongoose.connect("mongodb://127.0.0.1:27017/typelis")
     .then(db => console.log('Db is conected'))
-    .catch(error => console.log('Db is conected'))
+    .catch(error => console.log('Db is disconected'))
