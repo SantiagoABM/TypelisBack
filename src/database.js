b71746d984f9a9beb70fa54fe7ab683a06d63b15
SantiagoABM <barbozamujica109@gmail.com>
2024-01-25 10:51:23 -0500

    commits

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const MONGODB_URI = require("./config");
 
-mongoose.connect(MONGODB_URI)
+mongoose.connect("roundhouse.proxy.rlwy.net:46964" || "mongodb://127.0.0.1:27017/typelis")
     .then(db => console.log('Db is conected'))
     .catch(error => console.log('Db is disconected'))
