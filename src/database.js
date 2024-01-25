8e7166269ff9e21a43c64ef5f61c30d23d34307b
SantiagoABM <barbozamujica109@gmail.com>
2024-01-25 10:45:54 -0500

    commit

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const MONGODB_URI = require("./config");
 
-mongoose.connect("mongodb://127.0.0.1:27017/typelis")
+mongoose.connect(MONGODB_URI)
     .then(db => console.log('Db is conected'))
     .catch(error => console.log('Db is disconected'))
